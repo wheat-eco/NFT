5cd6c0bde30a04f183bdadb08c88e7f043dfdc09
wheat-eco <wheat.sol.eco@gmail.com>
2025-01-30 23:41:12 +0100

    update WalletConnect.tsx

--- sui/src/components/WalletConnect.tsx
+++ sui/src/components/WalletConnect.tsx
@@ -6,10 +6,9 @@ import CustomWalletProvider from "./WalletProvider";
 export default function WalletConnect() {
   return (
     <CustomWalletProvider>
-      <div className="flex flex-col items-center gap-4 p-4 border rounded-md shadow-md">
-        {/* The ConnectButton automatically handles the connection and disconnection */}
-        <ConnectButton />
-      </div>
-    </CustomWalletProvider>
+      <ConnectButton />
+     </CustomWalletProvider>
   );
 }
+
+
