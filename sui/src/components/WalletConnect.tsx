c826879936f47fa585dcd912f26b454b37776fc6
wheat-eco <wheat.sol.eco@gmail.com>
2025-01-30 23:17:56 +0100

    update WalletConnect.tsx

--- sui/src/components/WalletConnect.tsx
+++ sui/src/components/WalletConnect.tsx
@@ -1,41 +1,15 @@
 "use client";
 
-import { useCurrentAccount, useConnectWallet, useDisconnectWallet } from "@mysten/dapp-kit";
+import { ConnectButton } from "@mysten/dapp-kit";
 import CustomWalletProvider from "./WalletProvider";
-
-function WalletConnectContent() {
-  const account = useCurrentAccount();
-  const connectWallet = useConnectWallet();
-  const disconnectWallet = useDisconnectWallet();
-
-  return (
-    <div className="flex flex-col items-center gap-4 p-4 border rounded-md shadow-md">
-      {account ? (
-        <div>
-          <p>Connected: {account.address}</p>
-          <button
-            className="px-4 py-2 text-white bg-red-500 rounded hover:bg-red-600"
-            onClick={disconnectWallet}
-          >
-            Disconnect
-          </button>
-        </div>
-      ) : (
-        <button
-          className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600"
-          onClick={connectWallet}
-        >
-          Connect Wallet
-        </button>
-      )}
-    </div>
-  );
-}
 
 export default function WalletConnect() {
   return (
     <CustomWalletProvider>
-      <WalletConnectContent />
+      <div className="flex flex-col items-center gap-4 p-4 border rounded-md shadow-md">
+        {/* The ConnectButton automatically handles the connection and disconnection */}
+        <ConnectButton />
+      </div>
     </CustomWalletProvider>
   );
 }
