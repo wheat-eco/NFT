1941b1b8a3ccdf4bd8f1ccdb3d0fa52917d36ca0
wheat-eco <wheat.sol.eco@gmail.com>
2025-01-30 23:49:46 +0100

    update WalletProvider.tsx

--- sui/src/components/WalletProvider.tsx
+++ sui/src/components/WalletProvider.tsx
@@ -1,8 +1,16 @@
 // src/components/WalletProvider.tsx
 "use client";
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WalletProvider } from '@mysten/dapp-kit';
 
+// Create a single instance of QueryClient
+const queryClient = new QueryClient();
+
 export default function CustomWalletProvider({ children }: { children: React.ReactNode }) {
-  return <WalletProvider>{children}</WalletProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <WalletProvider>{children}</WalletProvider>
+    </QueryClientProvider>
+  );
 }
